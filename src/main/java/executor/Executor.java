8cf0f91d99b0989cba5e23f5e79e15afb1118a77
JPKingtero <JPQM4897@gmail.com>
2023-04-11 17:59:04 -0500

    refactor(main): the .gitignore document was added

--- src/main/java/executor/Executor.java
+++ src/main/java/executor/Executor.java
@@ -27,35 +27,28 @@ public class Executor {
              optionSelected = printer.mainMenu(scanner);
              switch (optionSelected){
                  case 1:
-                     //1.Print my current money amount
                      printer.currentMoneyAmount(account);
                      break;
                  case 2:
-                     //2.Print my current ingredient inventory.
                      printer.currentIngredientInventory(ingredientsAmounts,ingredientsNames);
                      break;
                  case 3:
-                     //3.Buy ingredients.
                      inventory.buyIngredients(scanner,account);
                      break;
                  case 4:
-                     //4.Print my dishes inventory.
                      printer.currentRecipeList();
                      break;
                  case 5:
-                     //5.Make a dish.
                      System.out.println("Please introduce the recipe ID:");
                      recipeID = scanner.nextInt();
                      order.makeADish(recipeID,inventory);
                      break;
                  case 6:
-                     //6.Sell a dish.
                      System.out.println("Please introduce the recipe ID:");
                      recipeID = scanner.nextInt();
                      account.Deposit(order.sellADish(recipeID));
                      break;
                  case 7:
-                     //7.EXIT.
                      break;
                  default:
                      System.out.println("Option not founded");
--- src/main/java/orders/OrderHistory.java
+++ src/main/java/orders/OrderHistory.java
@@ -2,5 +2,5 @@ package orders;
 
 public class OrderHistory {
 
-
+//Manejo de bases de H2
 }
